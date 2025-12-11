9d1289c55a2923776e4891db612242a94e6cb1c6
Raphaël Pinson <raphink@gmail.com>
2025-12-11 17:37:33 +0100

    lint fix Signed-off-by: Raphaël Pinson <raphink@gmail.com>

--- frontend/src/services/debateService.js
+++ frontend/src/services/debateService.js
@@ -47,6 +47,7 @@ export const generateDebateStream = (topic, selectedPanelists, onMessage, onErro
       const decoder = new TextDecoder();
       let buffer = '';
 
+      // eslint-disable-next-line no-constant-condition
       while (true) {
         const { done, value } = await reader.read();
 
