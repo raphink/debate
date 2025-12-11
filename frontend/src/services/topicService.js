2f379300efd78806910b51b0dd21277a76fe24c1
Raphaël Pinson <raphink@gmail.com>
2025-12-11 22:52:32 +0100

    lint
    Signed-off-by: Raphaël Pinson <raphink@gmail.com>

--- frontend/src/services/topicService.js
+++ frontend/src/services/topicService.js
@@ -40,6 +40,7 @@ export const validateTopic = async (
     const decoder = new TextDecoder();
     let buffer = '';
 
+    // eslint-disable-next-line no-constant-condition
     while (true) {
       const { done, value } = await reader.read();
 
