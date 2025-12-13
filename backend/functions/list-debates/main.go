f464a1c133b27163398f1943988a84036d0d9b6c
Raphaël Pinson <raphink@gmail.com>
2025-12-13 12:57:20 +0100

    fix
    Signed-off-by: Raphaël Pinson <raphink@gmail.com>

--- backend/functions/list-debates/main.go
+++ backend/functions/list-debates/main.go
@@ -1,6 +1,10 @@
 package listdebates
 
-import "net/http"
+import (
+	"net/http"
+
+	_ "github.com/GoogleCloudPlatform/functions-framework-go/funcframework"
+)
 
 // ListDebates is the Cloud Functions entry point
 func ListDebates(w http.ResponseWriter, r *http.Request) {
